0e3ca675673f7f3b7dbc7a4f18a78561fedf48e9
Lukasz Konrad <lukasz.konrad@gmail.com>
2022-12-28 11:16:25 +0100

    add function removeTask

--- script.js
+++ script.js
@@ -13,6 +13,20 @@
 
     ]
 
+    
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent
+        })
+        render();
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+                render();
+    }
+
+
     const render = () => {
         let htmlString = ""
 
@@ -20,20 +34,28 @@
             htmlString += `
             <li
             ${task.done ? "style=\"text-decoration: line-through\"": ""}>
-            <div class="add_task">${task.content}</div>
+
+            
+            <div class="add_task"><button class="js-remove">🗑</button>${task.content}</div>
             </li>
             `
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+
+        });
+
     };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent
-        })
-        render();
-    };
+
 
     const onFormSubmit = (event) => {
         event.preventDefault();
